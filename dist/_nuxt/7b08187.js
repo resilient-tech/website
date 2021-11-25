10447c0a4ea6c775fe2c606b72b39b93fbb56e73
Pruthvi Patel <pruthvipatel145@gmail.com>
2021-11-25 16:28:19 +0530

    Revert "publish dist folder"
    This reverts commit 54e6b6d2acd469778938c0b563e58f6b9f5e3259.

--- dist/_nuxt/7b08187.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{271:function(e,n,t){"use strict";t.r(n);var r=t(21),component=Object(r.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",{staticClass:"wrapper"},[e._v("This is careers page")])}),[],!1,null,null,null);n.default=component.exports}}]);
