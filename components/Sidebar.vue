3b103dde493119a18a0c5bfc4f031b413fbe2189
Pruthvi Patel <pruthvipatel145@gmail.com>
2021-11-25 16:53:15 +0530

    fix: favicon, bg color

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -3,10 +3,10 @@
     <div class="inner">
       <nav>
         <ul>
-          <li><a href="#intro">Welcome</a></li>
-          <li><a href="#erpnext">ERPNext</a></li>
-          <li><a href="#services">Our Services</a></li>
-          <li><a href="#contact">Get in touch</a></li>
+          <li><a href="#intro" class="nav-item">Welcome</a></li>
+          <li><a href="#erpnext" class="nav-item">ERPNext</a></li>
+          <li><a href="#services" class="nav-item">Our Services</a></li>
+          <li><a href="#contact" class="nav-item">Get in touch</a></li>
           <li>
             <NuxtLink to="/careers" class="nuxt-link">
               <span class="button fit careers"> Careers </span>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500',
